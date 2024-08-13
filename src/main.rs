f0d042fb2e65670269e47d18d60ea637e9e0f1bd
AvaterClasher <avater.clasher@gmail.com>
2024-08-13 23:30:10 +0530

    feat: added meta commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-use std::{env, io::{stdout,stdin,Write}};
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+};
+
+enum MetaCommandResult {
+    Success(String),
+    Unknown,
+}
+
+fn handle_commands(cmd: &String) -> MetaCommandResult {
+    match cmd.starts_with(".") {
+        true => MetaCommandResult::Success(cmd.to_owned()),
+        false => MetaCommandResult::Unknown,
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -6,15 +21,18 @@ fn main() {
     for arg in args {
         println!("{}", arg);
     }
-    loop{
+    loop {
         print!("rdb> ");
-        let _ = stdout().flush();
-        stdin().read_line(&mut command).expect("Error while trying to read from stdin");
-        if command.trim() == "exit" {
-            break;
-        }
-        else {
-            println!("Command not found");
+        stdout().flush().unwrap();
+        stdin()
+            .read_line(&mut command)
+            .expect("Error while trying to read from stdin");
+        match handle_commands(&command.trim().to_owned()) {
+            MetaCommandResult::Success(cmd) => match cmd.as_ref() {
+                ".exit" => break,
+                _ => println!("Unrecognized meta command {}", cmd),
+            },
+            MetaCommandResult::Unknown => println!("Unrecognized command!"),
         }
         command.clear();
     }
